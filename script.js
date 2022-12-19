907d7aa32184ca04de3c439a189dadfb49766c19
Kyle Simmons <simmons.kyle.b@gmail.com>
2022-12-19 13:39:34 -0700

    generates random garbage

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-// Assignment code here
+const caseLower = "abcdefghijklmnopqrstuvwxyz"
+const caseUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const validNums = "1234567890"
+const validSymbols = "!@#$%^&*()_+-=/\\[]{}?.,><:;'"
+const mainInterface = document.getElementById("inputInterface")
+const generateBtn = document.querySelector("#generate")
+const passwordText = document.querySelector("#password")
+generateBtn.addEventListener("click",parseInput)
 
+function parseInput(){
+  i = document.getElementById("pwLength")
+  if (i.value<8 || i.value>16){
+    alert("Length must be between 8 and 16")
+  }
+  j = document.getElementById("useOption")
+  k = document.getElementById("stringOption")
+selectedArray = (j.value=="useBinary")
+    ? "01"
+  :(j.value=="useHex")
+    ? validNums + "ABCDEF"
+    :selectedArray
+  switch(k.value){
+    case("useLower"):
+    selectedArray += caseLower
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+    case("useUpper"):
+    selectedArray += caseUpper
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    case("useSymbol"):
+    selectedArray += validSymbols
 
-  passwordText.value = password;
+    case("useNums"):
+    selectedArray += validNums
 
+    default:
+    selectedArray = "test"
+  }
+  var char
+  for (t=0; t<i.value; t++){
+    char+=selectedArray[Math.floor(Math.random()*i.value+1)]
+  }
+  passwordText.value = char
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  
