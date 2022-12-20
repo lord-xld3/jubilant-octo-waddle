b3e260bf20c24e031eaf355295292c4ffedab991
Kyle Simmons <simmons.kyle.b@gmail.com>
2022-12-19 18:11:29 -0700

    Able to generate longer passwords, can use unicode but does not display in raw text box. Should also set default button selections.

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
-const caseLower = "abcdefghijklmnopqrstuvwxyz"
-const caseUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const validNums = "1234567890"
-const validSymbols = "!@#$%^&*()_+-=/\\[]{}?.,><:;'"
-const mainInterface = document.getElementById("inputInterface")
-const generateBtn = document.querySelector("#generate")
-const passwordText = document.querySelector("#password")
+const caseLower="abcdefghijklmnopqrstuvwxyz"
+const caseUpper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const validNums="1234567890"
+const validSymbols=" `~!@#$%^&*()_+-=/\\[]{}?.,><:;\'\""
+const mainInterface=document.getElementById("inputInterface")
+const generateBtn=document.querySelector("#generate")
+const passwordText=document.querySelector("#password")
 generateBtn.addEventListener("click",parseInput)
 
 function parseInput(){
-  var inputLength = document.getElementById("pwLength")
-  if (inputLength.value<1 || inputLength.value>16){
-    alert("Length must be between 1 and " + 16)
+  //Error handling
+  var inputLength=document.getElementById("pwLength")
+  if (inputLength.value<1 || inputLength.value>20000000){
+    alert("Length must be between 1 and " + 20000000)
   }
-  var useBin = document.getElementById("useBin")
-  var useHex = document.getElementById("useHex")
-  var useLower = document.getElementById("useLower")
-  var useUpper = document.getElementById("useUpper")
-  var useSymbols = document.getElementById("useSymbols")
-  var useNums = document.getElementById("useNums")
 
+  //Get values from elements
+  var useBin=document.getElementById("useBin")
+  var useHex=document.getElementById("useHex")
+  var useUni=document.getElementById("useUni")
+  var stringLower=document.getElementById("stringLower")
+  var stringUpper=document.getElementById("stringUpper")
+  var stringSymbols=document.getElementById("stringSymbols")
+  var stringNums=document.getElementById("stringNums")
+  var password=""
+
+  //Unicode generator
+  if (useUni.checked) {
+    for (t=0; password.length<inputLength.value*7; t++){
+      var uniChar=(Math.floor(Math.random()*65535)).toString(16)
+      while (uniChar.length<4) {
+        uniChar+="0"
+      }
+      password+="&#" + uniChar + ";"
+    }
+    return passwordText.value = password
+  }
+
+  //Conditional logic on values to determine which character set to use
   var selectedArray=(useBin.checked)?"10":(useHex.checked)?validNums + "abcdef":""
-  selectedArray+=(useLower.checked)?caseLower:""
-  selectedArray+=(useUpper.checked)?caseUpper:""
-  selectedArray+=(useSymbols.checked)?validSymbols:""
-  selectedArray+=(useNums.checked)?validNums:""
+  selectedArray+=(stringLower.checked)?caseLower:""
+  selectedArray+=(stringUpper.checked)?caseUpper:""
+  selectedArray+=(stringSymbols.checked)?validSymbols:""
+  selectedArray+=(stringNums.checked)?validNums:""
   selectedArray=(selectedArray=="")?validNums:selectedArray
   Array.from(selectedArray)
 
-  if (useBin.checked || useHex.checked) {
+  //Optimization for binary/hex/num passwords less than 16 chars long
+  if (inputLength.value*1<17 && (useBin.checked || useHex.checked )) {
     var rngStr=""
     for (t=0; rngStr.length<1+inputLength.value*1; t++){
-      rngStr += (Math.floor(Math.random()*(1+inputLength.value*1))+"")
+      rngStr+=(Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)+"")
     }
     rngStr=rngStr*1
-    var password = (selectedArray=="10")
-    ?rngStr.toString(2)
-      :(selectedArray=="1234567890abcdef")
-    ? rngStr.toString(16)
-      :(selectedArray==validNums)
-    ? password = rngStr.toString()
-      :password
+    var password=(selectedArray=="10")?rngStr.toString(2)
+    :(selectedArray=="1234567890abcdef")?rngStr.toString(16)
+    :(selectedArray==validNums)?password=rngStr.toString()
+    :password
     Array.from(password)
     return passwordText.value = password.slice(1,1+inputLength.value*1)
   }
-  var password=""
+
+  //Default password generator
   for(t=0; password.length<inputLength.value; t++){
-    password += selectedArray[(Math.floor(Math.random()*(selectedArray.length))+"")]
+    password+=selectedArray[(Math.floor(Math.random()*(selectedArray.length))+"")]
   }
   return passwordText.value = password
 }
